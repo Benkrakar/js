8d92e243a237db017e0189f989b5dd4f8fbb91de
unknown <abdelazizbardich@gmail.com>
2021-02-15 16:33:32 +0100

    merging services, about, home & header sections into main

--- website/js/script.js
+++ website/js/script.js
@@ -1,26 +1,63 @@
 //slide team//
-var $owl = $('.owl-carousel');
-
+let $owl = $('#owl-carousel-1');
 $owl.children().each( function( index ) {
-  $(this).attr( 'data-position', index ); // NB: .attr() instead of .data()
+  $(this).attr( 'data-position', index );
 });
-
 $owl.owlCarousel({
   center: true,
   loop: true,
   items: 3,
   nav:false,
   autoplay:true,
-  dots:true
+  dots:true,
+  responsive : {
+    0 : {
+        items:1
+    },
+    480 : {
+        items:1
+    },
+    768 : {
+        items:3
+    }
+}
 });
-
 $(document).on('click', '.owl-item>div', function() {
-  // see https://owlcarousel2.github.io/OwlCarousel2/docs/api-events.html#to-owl-carousel
-  var $speed = 300;  // in ms
+  var $speed = 300;
   $owl.trigger('to.owl.carousel', [$(this).data( 'position' ), $speed] );
 });
-//slide end//
-
+$('.img-team').css({
+    'max-height':$('.img-team').width()
+});
+// Slider clients
+//slide team//
+$owl = $('#owl-carousel-2');
+$owl.children().each( function( index ) {
+  $(this).attr( 'data-position', index );
+});
+$owl.owlCarousel({
+  center: true,
+  loop: true,
+  items: 5,
+  nav:false,
+  autoplay:true,
+  dots:true,
+  responsive : {
+    0 : {
+        items:1
+    },
+    480 : {
+        items:1
+    },
+    768 : {
+        items:5
+    }
+}
+});
+$(document).on('click', '.owl-item>div', function() {
+  var $speed = 300;
+  $owl.trigger('to.owl.carousel', [$(this).data( 'position' ), $speed] );
+});
 $('.img-team').css({
     'max-height':$('.img-team').width()
 });
