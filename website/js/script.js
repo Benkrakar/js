b02b50a7f62025b8d0a29136351c3940172fe802
EL MANTAGUI Abdelali <alix123e@gmail.Com>
2021-02-16 11:22:30 +0100

    counter effect

--- website/js/script.js
+++ website/js/script.js
@@ -42,7 +42,7 @@ $owl.owlCarousel({
     nav: true,
     autoplay: true,
     dots: true,
-    transitionStyle : "fade",
+    transitionStyle: "fade",
     responsive: {
         0: {
             items: 1
@@ -63,3 +63,29 @@ $('.img-team').css({
     'max-height': $('.img-team').width()
 });
 
+function domCounter(element, number, duration) {
+    let count = 0;
+    const intervalDuration = duration / (number / 100)
+    const interval = setInterval(() => {
+        if (count <= number) {
+            element.innerText = parseInt(count);
+        } else {
+            element.innerText = parseInt(number);
+            clearInterval(interval)
+        }
+        const willAdd = Math.floor(Math.random() * number / 100);
+        count += willAdd;
+    }, intervalDuration);
+}
+let isCounted = false
+window.addEventListener('scroll', function() {
+    const learningSection = document.querySelector(".n-s-learning");
+    console.log('learningSection.getBoundingClientRect().top', learningSection.getBoundingClientRect().top);
+    if (isCounted == false && (learningSection.getBoundingClientRect().top >= 0 && learningSection.getBoundingClientRect().bottom <= window.innerHeight)) {
+        isCounted = true;
+        domCounter($(".fa-download + a")[0], 4342);
+        domCounter($(".fa-award + a")[0], 1533);
+        domCounter($(".fa-heart + a")[0], 23423);
+        domCounter($(".fa-star + a")[0], 1723);
+    }
+})
